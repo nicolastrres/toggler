b56585f3e848d91169ac74511ed31215ad446bb9
Nicolas Agustin <nicolastrres@gmail.com>
2019-02-08 07:12:33 -0300

    Adds db client

---
+++ toggler/clients/dbClient.js
@@ -0,0 +1,24 @@
+const AWS = require('aws-sdk')
+const { map, prop, pick } = require('ramda')
+
+AWS.config.update({
+  region: 'us-east-1'
+})
+
+const getAllItems = (tableName) => {
+  const client = new AWS.DynamoDB.DocumentClient()
+
+  return client.scan({ TableName: tableName }).promise()
+    .then(prop('Items'))
+}
+
+const addItem = (tableName, item) => {
+  const client = new AWS.DynamoDB.DocumentClient()
+
+  return client.put({
+    TableName: tableName,
+    Item: item
+  })
+}
+
+module.exports = { getAllItems, addItem }
---
+++ toggler/tests/clients/dbClient.test.js
@@ -0,0 +1,36 @@
+const { expect } = require('chai')
+const { addItem, getAllItems } = require('../../clients/dbClient')
+const { spy } = require('sinon')
+const path = require('path')
+
+const AWS = require('aws-sdk-mock')
+AWS.setSDK(path.resolve(__dirname, '../../node_modules/aws-sdk'))
+
+
+describe('dbClient', () => {
+  let putSpy
+
+  before(() => {
+    putSpy = spy()
+    AWS.mock('DynamoDB.DocumentClient', 'scan', { Items: 'someItems' })
+    AWS.mock('DynamoDB.DocumentClient', 'put', putSpy)
+  })
+
+  it('gets all items', async () => {
+    const expectedItems = 'someItems'
+
+    const items = await getAllItems('someTable')
+
+    expect(items).to.be.equal(expectedItems)
+  })
+
+  it('adds an item', async () => {
+    await addItem('someTable', 'someItem')
+
+    expect(putSpy.calledOnceWith({
+      Item: 'someItem',
+      TableName: 'someTable'
+    })).to.be.equal(true)
+
+  })
+})
